e0da2aa52f7a3233233b821972fed1882a75e38f
Andrew Zyabin [🔒] <schas002@users.noreply.github.com>
2017-11-01 13:59:30 +0400

    :sparkles: add _Hello, World!_

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # NaNoGenMo-2017
-Efforts to make at least *something* for NaNoGenMo 2017
+
+> Efforts to make at least *something* for NaNoGenMo 2017
+
+## Install
+
+All stories are self-contained. Nothing to do here.
+
+## Usage
+
+Where `story` is any story Python file in this repository:
+
+```sh
+$ python3 story  # generates random reproducible seed
+$ python3 story 0.1234567890  # with seed
+```
+
+## Stories
+
+### Hello, World! _(hello-world)_
+
+> A parade of eponymous phrases for NaNoGenMo 2017
+
+Both a test to my [story boilerplate](story-boilerplate/story.py) and a standalone story, because [gladiolus](https://github.com/NaNoGenMo/2017/blob/master/README.md#the-rules). 😜
+
+> hello, world!
+>
+> hello, World.
+>
+> hello world
+>
+> Hello world
+>
+> Hello, World!
+
+## Maintainer
+
+- Andrew Zyabin - @schas002 - 2 previous Na*GenMo works
+
+## License
+
+[MIT](LICENSE) &copy; Andrew Zyabin
---
+++ hello-world/hello-world.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+import datetime
+import re
+
+# # #
+
+NAME = 'Hello, World!'
+PREAMBLE = 'A parade of eponymous phrases for NaNoGenMo 2017'
+AUTHORED_BY = 'Andrew Zyabin'
+
+# # #
+
+def para(*objects, sep=' ', flush=False):
+	print(*objects, sep=sep, end='\n\n', file=sys.stdout, flush=flush)
+
+# # #
+
+seed = sys.argv[1] if len(sys.argv) > 1 else str(random.random())
+
+rn = random.Random(seed)
+
+grammar = {
+	'*': ['{{hello}} {{world}}'],
+	'hello': ['Hello', 'hello', 'Hello,', 'hello,'],
+	'world': ['World', 'world', 'World!', 'world!', 'World.', 'world.']
+}
+
+# # #
+
+para('#', '_' + NAME + '_')
+para(PREAMBLE)
+para('Authored by', AUTHORED_BY)
+para('Reproducible seed:', seed)
+para('Last update:', datetime.date.today().isoformat())
+para('* * *')
+
+# # #
+
+words = 0
+
+while words < 50000:
+	p = rn.choice(grammar['*'])
+	while True:
+		match = re.search('{{(.+?)}}', p)
+		if match:
+			p = p.replace(match[0], rn.choice(grammar[match[1]]))
+		else:
+			break
+	para(p)
+	words += len(p.split())
